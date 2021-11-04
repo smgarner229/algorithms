7c56f96a6b926b9404dac0bd64a9d23d29e2440c
smgarner <smgarner@berkeley.edu>
2021-11-04 08:55:09 -0700

    Work on selection sort

--- section_one/selection_sort.c
+++ section_one/selection_sort.c
@@ -11,29 +11,52 @@ static void swap(void * a,
                  void * b,
                  size_t data_size)
 {
-    //memcpy from a into temp
-    //memcpy from b into a
-    //memcpy from temp into b
     char temp[data_size];
-    //void * temp = malloc(data_size);
-    //void*temp=memcpy(malloc(data_size),a,data_size);
     memcpy(temp,a,data_size);
     memcpy(a,b,data_size);
     memcpy(b,temp,data_size);
-    //free(temp);
-    
+    return;
 }
 
 /**
  * @brief Selection sort algorithm
  * 
- *
+ * @param data pointer to start of array of elements
+ * @param n_elem number of elements
+ * @param data_size size of the data type being sorted in bytes
+ * @param compare.  takes void pointer to the data type, and determines which is larger. Returns <=0 if the first argument should come first, > 0 if the second
  */
 void * selection_sort(void * data, 
                       size_t n_elem, 
                       size_t data_size,
                       int (*compare)(void*,void*))
 {
+    char key[data_size];
+    int j = 0;
+    for(int i = 1; i < n_elem; i++)
+    {
+        memcpy(key,((char*)data)+i*data_size,data_size);
+        fprintf(stdout,"Current contents of key:\t%d\n",*(int*)key);
+        j = i - 1;
+        fprintf(stdout,"First compare gives:\t%d\n",compare(((char*)data)+j*data_size,key) );
+        while(j>0 & compare(((char*)data)+j*data_size,key) > 0)
+        {
+            swap(((char*)data)+(j+1)*data_size,((char*)data)+(j)*data_size,data_size);
+            j--;
+        }
+        memcpy(((char*)data)+(j+1)*data_size,key,data_size);
+        for(int i = 0; i < 5; i++)
+            fprintf(stdout,"%d\t",*(((int*)data)+i));
+        fprintf(stdout,"\n");
+    }
+}
+
+
+int comp_ints(void * a, void * b)
+{
+    int left = *(int *)a;
+    int right = *(int *)b;
+    return left - right;
 }
 
 int main(int argc, char ** argv)
@@ -45,6 +68,18 @@ int main(int argc, char ** argv)
 //    a[0] = 10;
 //    a[1] = 20;
 //    my_ptr = a;
+
+    int sortme[5]={2,5,3,1,4};
+    for(int i = 0; i < 5; i++)
+        fprintf(stdout,"%d\t",sortme[i]);
+    fprintf(stdout,"\n");
+
+    selection_sort(sortme,5,sizeof(int),&comp_ints);
+
+    for(int i = 0; i < 5; i++)
+        fprintf(stdout,"%d\t",sortme[i]);
+    fprintf(stdout,"\n");
+
     int * a, * b;
     a = malloc(sizeof(int));
     b = malloc(sizeof(int));
