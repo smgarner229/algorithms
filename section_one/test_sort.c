9cbf8e2b43d81907cc3c7892078125aba16daaac
Scott Garner <smgarner@berkeley.edu>
2022-03-22 00:16:36 -0700

    Test suite is now struct with function pointers

--- section_one/test_sort.c
+++ section_one/test_sort.c
@@ -5,57 +5,83 @@
 #include "search.h"
 #include "person.h"
 
+typedef int(*sort_criterea)(const void *, const void *);
+
+struct _test_functions{
+  int size;
+  int heap_size;
+  sort_criterea * funcs;  
+  char ** criterea;
+};
+typedef struct _test_functions test_functions;
+
+void init_tests(test_functions * suite)
+{
+  suite->size = 0;
+  suite->heap_size = 1;
+  suite->funcs = (sort_criterea*)malloc(sizeof(sort_criterea)*suite->heap_size);
+  suite->criterea = (char**)malloc(sizeof(char*)*suite->heap_size);
+}
+
+void add_test(test_functions * suite, const sort_criterea sort_function, const char * criterea)
+{
+  if(suite->size >= suite->heap_size)
+  {
+    suite -> heap_size *= 2;
+    suite -> funcs = (sort_criterea*)realloc(suite->funcs,sizeof(sort_criterea)*suite->heap_size);
+    suite -> criterea = (char**)realloc(suite->criterea,sizeof(char*)*suite->heap_size);
+  }
+  suite -> funcs[suite->size] = sort_function;
+  suite -> criterea[suite->size] = strdup(criterea);
+  suite -> size++;
+}
+
+void cleanup_test(test_functions * suite)
+{
+  free(suite->funcs);
+  for(int i = 0; i < suite->size; i++)
+  {
+    free(suite->criterea[i]);
+  }
+  free(suite->criterea);
+  return;
+}
+
+//void run_tests()
 
 int main(int argc, char**argv)
 {
-    Person * people = malloc(sizeof(Person)*4);
+    const int n_people = 7;
+    test_functions * test_suite = malloc(sizeof(test_functions));
+    init_tests(test_suite);
+
+    add_test(test_suite,increasing_age,"age");
+    add_test(test_suite,alphabetically,"name");
+    add_test(test_suite,decreasing_age,"backward age");
+    add_test(test_suite,increasing_height,"height");
+    
+    Person * people = malloc(sizeof(Person)*n_people);
     init_person(people,"Rich",45,2.0);
     init_person(people+1,"Sally",8,1.0);
     init_person(people+2,"Alice",88,1.5);
     init_person(people+3,"Bob",25,2.2);
+    init_person(people+4,"Scort",26,2.2);
+    init_person(people+5,"Norg",23,1.8);
+    init_person(people+6,"Leo",26,2.0);
 
-    typedef int(sort_criterea)(const void *, const void *);
-
-    sort_criterea * sort_list = malloc(sizeof(sort_criterea)*4);
-
-//    sort_criterea sort_list[4];
-    
-    sort_list = &increasing_age;
-    sort_list++;
-    sort_list + 1 = alphabetically;
-    sort_list++;
-    sort_list = &decreasing_age;
-    sort_list++;
-    sort_list = &increasing_height;
-    sort_list-=3;
-/*
-    sort_list[0] = &increasing_age;
-    sort_list[1] = alphabetically;
-    sort_list[2] = &decreasing_age;
-    sort_list[3] = &increasing_height;
-    */
-
-    for(int i = 0; i < 4; i++)
+    for(int i = 0; i < test_suite->size; i++)
     {
-        insertion_sort(people,4,sizeof(Person),sort_list++);
-        print_ordered(people,4,"idk");
+	merge_sort(people,n_people,sizeof(Person),test_suite->funcs[i]);
+//#insertion_sort(people,n_people,sizeof(Person),test_suite->funcs[i]);
+        print_ordered(people,n_people,test_suite->criterea[i]);
     }
+    
 
-    print_ordered(people,4,"nothing");
- 
-    insertion_sort(people,4,sizeof(Person),&increasing_age);
-    print_ordered(people,4,"increasing age");
-
-    insertion_sort(people,4,sizeof(Person),&alphabetically);
-    print_ordered(people,4,"alphabetically");
-
-    insertion_sort(people,4,sizeof(Person),&decreasing_age);
-    print_ordered(people,4,"decreasing age");
-
-    insertion_sort(people,4,sizeof(Person),&increasing_height);
-    print_ordered(people,4,"height");
-
+    for(int i = 0; i < n_people; i++)
+    {clear_person(people+i);}
     free(people);
 
+    cleanup_test(test_suite);
+    free(test_suite);
 }
 
